2f19a0d280d66469db44e4b17cea3c35b0ba6020
Rob Harrison <rob@appinstitute.co.uk>
2014-12-04 18:55:12 +0000

    Fixed potential interferance with session based login pages on same server as memcached dashboard.

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ class Simple_memchached_dashboard{
     }
 
     function is_logged_in(){
-		return (isset($_SESSION['username']) && isset($this->users[$_SESSION['username']]) && $_SESSION['username'] != '');
+		return (isset($_SESSION['memcached_dashboard_username']) && isset($this->users[$_SESSION['memcached_dashboard_username']]) && $_SESSION['memcached_dashboard_username'] != '');
 	}
 
 	function add_user($user_name,$user_pass){
@@ -97,7 +97,7 @@ class Simple_memchached_dashboard{
 
 	function logout(){
 		if(isset($_GET['action']) && $_GET['action'] == 'logout') {
-			unset($_SESSION['username']);
+			unset($_SESSION['memcached_dashboard_username']);
 			session_write_close();
 			header('Location:  ' . $_SERVER['PHP_SELF']);
 		}
@@ -110,7 +110,7 @@ class Simple_memchached_dashboard{
 			$pass = $_POST['password'];
 			if ($this->user_exists($user)){
 				if($pass == $this->get_user_pass($user)) {
-					$_SESSION['username'] = $_POST['username'];
+					$_SESSION['memcached_dashboard_username'] = $_POST['username'];
 				}else {
 					$this->error = 'Wrong Password!';
 				}
